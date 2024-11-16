b8df48b2b4c4a237982359b283d25361c4b871ea
Akhlaque <akhlaquea01@gmail.com>
2024-11-16 18:19:33 +0530

    list fixed

--- mini-game/game-list.js
+++ mini-game/game-list.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Get the game items by their IDs
     const catchGameBtn = document.getElementById('catch-game');
-    const game2Btn = document.getElementById('game2');
+    // const game2Btn = document.getElementById('game2');
 
     // Event listener for Catch the Falling Object game
     catchGameBtn.addEventListener('click', () => openGame('Catch-the-Falling-Object/game.html'));
 
     // Event listener for Game 2
-    game2Btn.addEventListener('click', () => openGame('game2.html'));
+    // game2Btn.addEventListener('click', () => openGame('game2.html'));
 
     // Function to navigate to the selected game page
     function openGame(gameName) {
